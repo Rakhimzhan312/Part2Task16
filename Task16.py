3b5f2e86ba996a14365de225904251fae556cdec
Rakhimzhan <rakhimzhan312@gmail.com git config --global user.name Rakhimzhangit push -u origin mastergit statusgit push origin mastergit config --global user.email rakhimzhan312@gmail.com>
2019-11-22 19:30:35 +0600

    Part2Task16

---
+++ Task16.py
@@ -0,0 +1,8 @@
+zemnoives = int(input('Введите Ваш вес на земле:'))
+for i in range(1, 16):
+    ves = 0.165*zemnoives
+    zemnoives+=1
+    print('Вес на луне каждый год, при условии если Вы набрали 1кг в год', ves)
+
+
+
